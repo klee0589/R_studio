2174af13061c727352cae5e7a1e31e6535f1969e
Kangil Lee <kangil.lee@fanduel.com>
2020-11-02 11:58:40 -0500

    add lotto csv

--- Testing.R
+++ Testing.R
@@ -1,3 +1,14 @@
-print("This file was created within RStudio")
+first <- c(1:69)
+second <- c(2:69)
+third <- c(3:69)
+fourth <- c(4:69)
+fifth <- c(5:69)
 
-print("And now it lives on GitHub")
+power <- c(1:26)
+
+x <- data.frame(first = 1:69, second = 2:70, third = 3:71, fourth = 4:72, fifth = 5:73)
+
+y <- read.csv(file = "/Users/kangillee/downloads/Lottery_Powerball_Winning_Numbers__Beginning_2010.csv")
+
+print(y)
+                                         
